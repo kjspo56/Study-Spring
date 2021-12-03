570c3c19a115b5c03d170da6837dfd383c63c4e5
Roma_Man <62382377+kjspo56@users.noreply.github.com>
2021-12-03 10:29:41 +0900

    Delete Main.java

--- 210817/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args)	{
-        MyFrame frame = new MyFrame();
-        frame.setSize(600, 600);
-        frame.setVisible(true);
-    }
-}
